9a280e1beb4d31914e7117144bcd8c13cb0dd0d7
bablucs12 <43750509+bablucs12@users.noreply.github.com>
2019-10-13 19:53:31 +0530

    Update socialnetwork.cpp

--- socialnetwork.cpp
+++ socialnetwork.cpp
@@ -5,23 +5,28 @@ using namespace std;
 #define come_on ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0); //___________program booster_________
 #define pop pop_back
 #define str string
+#define ll long long int
 #define endl "\n"
 #define vec vector
 #define m_p make_pair
 #define mod 1000000007
 main()
 {
-    int n,i,j,k;
+    ll n,i,j,k;
+    str s1,s2,s3,s4,s5,s6,s7;
+    ll arrwww[101];
+    for(i=0;i<10;i++) arrwww[i]=0;
     cin>>n>>k;
-    int arr[n];
-    map<int,int>mp;
-    list<int>q;
+    ll arr[n];
+    map<ll,ll>mp;
+    list<ll>q;
     //Running For Loop
     for(i=0;i<n;i++)
     {
         cin>>arr[i];
         if(mp.find(arr[i])==mp.end())
             mp[arr[i]]=0;
+        
         if(mp[arr[i]]==0)
         {
             if(q.size()<k)
@@ -29,18 +34,12 @@ main()
                 q.push_front(arr[i]);
                 mp[arr[i]]=1;
             }
-            else if(q.size()==k)
-            {
-                q.push_front(arr[i]);
-                mp[q.back()]=0;
-                mp[arr[i]]=1;
-                q.pop_back();
- 
-            }
+            
         }
  
     }
-    cout<<q.size()<<endl;
+    
+    cout<<q.size()<<"\n";
     for(auto it=q.begin();it!=q.end();it++)
     {
         cout<<*it<<" ";
